2685dc2cd1e895e3823096cd94c7798ad11603b5
Himanshu Gupta <44115296+himanshugupta30062@users.noreply.github.com>
2025-07-16 18:13:17 +0530

    feat: show Book Expert chatbot

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ChatbotProvider } from "@/contexts/ChatbotContext";
+import Chatbot from "@/components/chatbot/Chatbot";
 import { HelmetProvider } from 'react-helmet-async';
 import Navigation from "@/components/Navigation";
 import GlobalFooter from "@/components/GlobalFooter";
@@ -141,6 +142,7 @@ const App = () => (
                   <GlobalFooter />
                 </div>
                 <BackToTopButton />
+                <Chatbot />
               </BrowserRouter>
             </TooltipProvider>
           </ChatbotProvider>
