aa32ad2e645a617de6e29c83008413c5c498b476
Himanshu Gupta <44115296+himanshugupta30062@users.noreply.github.com>
2025-10-03 10:27:58 +0530

    Update maps.ts

--- src/config/maps.ts
+++ src/config/maps.ts
@@ -2,7 +2,7 @@
 // This is a publishable key that's meant to be used in the browser
 // Make sure to restrict it to your domain in Google Cloud Console
 
-export const GOOGLE_MAPS_API_KEY = 'YOUR_GOOGLE_MAPS_API_KEY_HERE';
+export const GOOGLE_MAPS_API_KEY = 'AIzaSyCw2_r9mOx7ORFN1i48tiKsjFKG4IUym2g';
 
 // Instructions to get your API key:
 // 1. Go to https://console.cloud.google.com/
